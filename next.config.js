169e26589ec664607facae7f4783b71a7d431753
TamemJalallar <tamem.jalallar@ogilvy.com>
2025-12-23 14:06:40 -0500

    Refactor next.config.js for GitHub Pages support

--- next.config.js
+++ next.config.js
@@ -1,16 +1,32 @@
 /** @type {import('next').NextConfig} */
+const repo = ""; // <-- leave "" if using <username>.github.io repo, otherwise set to your repo name
+
 const nextConfig = {
+  // If this template uses the /app directory, keep this:
   experimental: {
     appDir: true,
+    jsonModules: true
   },
-  images: {
-    domains: ['res.cloudinary.com', 'firebasestorage.googleapis.com', 'img.icons8.com', 'raw.githubusercontent.com', 'i.imgur.com', 'img.freepik.com','media.geeksforgeeks.org']
-  }
-}
 
-module.exports = {
-  experimental: {
-    jsonModules: true,
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "firebasestorage.googleapis.com",
+      "img.icons8.com",
+      "raw.githubusercontent.com",
+      "i.imgur.com",
+      "img.freepik.com",
+      "media.geeksforgeeks.org"
+    ]
   },
+
+  // ✅ Required for GitHub Pages static hosting:
+  output: "export",
+
+  // ✅ Only needed if you're deploying to https://username.github.io/REPO
+  ...(repo
+    ? { basePath: `/${repo}`, assetPrefix: `/${repo}/` }
+    : {})
 };
 
+module.exports = nextConfig;
