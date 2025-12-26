a84edeee05c5cffbb4f67b58a04852507a1caa86
TamemJalallar <tamem.jalallar@student.shu.edu>
2025-12-26 09:56:01 -0500

    Delete CallToAction.tsx

--- components/CallToAction.tsx
+++
@@ -1,9 +0,0 @@
-import { useTheme } from "next-themes"
-import Image from "next/image"
-import Link from "next/link"
-import { BiLinkExternal } from "react-icons/bi"
-import { FaGithub } from "react-icons/fa"
-import SectionWrapper from "./SectionWrapper"
-
-
-export default CallToAction
