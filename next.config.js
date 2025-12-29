cffc7fb059d0d754b3823dfa52343388cf823dce
TamemJalallar <tamem.jalallar@ogilvy.com>
2025-12-29 12:49:17 -0500

    Refactor next.config.js for GitHub Pages settings

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_ACTIONS === "true";
-const repo = "TamemJalallar.github.io"; // repo name
-
 const nextConfig = {
   output: "export",
-  images: { unoptimized: true },
   trailingSlash: true,
-  basePath: isGithubPages ? `/${repo}` : "",
-  assetPrefix: isGithubPages ? `/${repo}/` : "",
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
