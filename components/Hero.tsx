56c2a63b46bab3d5e0e30063e72bade0c4eb22a9
TamemJalallar <tamem.jalallar@student.shu.edu>
2025-12-29 12:44:41 -0500

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -105,7 +105,7 @@ const Hero = ({ mainData }: HeroProps) => {
               </ScrollLink>
 
               <a
-                href="/Tamem_Jalallar_IT_Resume.docx"
+                href="/Public/Resume.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="w-fit text-sm md:text-base py-3 px-5 inline-flex items-center gap-2 rounded-2xl bg-white/70 dark:bg-white/5 shadow-soft"
